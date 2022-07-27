c540e819325d676b986ef1f94c44b356314666e5
Carlos Valverde <z72vamac@uco.es>
2022-07-27 12:01:48 +0200

    Volvemos al modelo anterior por la memoria de los ordenadores y la pesadez del modelo.

--- routine_nolazy_A4.py
+++ routine_nolazy_A4.py
@@ -15,7 +15,7 @@ import networkx as nx
 import math
 import numpy as np
 import pandas as pd
-from h_kmedian_n_moresubindex import h_kmedian_n
+from h_kmedian_n import h_kmedian_n
 from neighborhood import Circle
 
 # Parameters of the model
@@ -27,8 +27,6 @@ for nn in n_Ns:
     k = [1, math.floor(0.1*nn), math.floor(0.25*nn)]
     ks.append(k)
 
-endurances = [100, 200, 500]
-
 time_limit = 3600
 
 wE = 1
@@ -37,28 +35,26 @@ wL = 50
 
 init = False
 
-dataframe = pd.DataFrame(columns=['Instance', 'n_N', 'n_B', 'k', 'Endurance', 'wE', 'wL', 'Gap', 'Runtime', 'NodeCount', 'ObjVal', 'Runtime_h', 'ObjVal_h'])
+dataframe = pd.DataFrame(columns=['Instance', 'n_N', 'n_B', 'k', 'wL', 'Gap', 'Runtime', 'NodeCount', 'ObjVal', 'Runtime_h', 'ObjVal_h'])
 
 for instance in instances:
     for nn, k_list in zip(n_Ns, ks):
         for k in k_list:
-            for endurance in endurances:
+            print('\n\nResolviendo la instancia ' + str(instance) + ' con un numero ' + str(nn) + ' de neighbourhoods.\n\n')
 
-                print('\n\nResolviendo la instancia ' + str(instance) + ' con un numero ' + str(nn) + ' de neighbourhoods.\n\n')
+            segments = np.genfromtxt('./instancias/segmentos' + str(nn) + '-' + str(instance) + '.csv', delimiter=',')
 
-                segments = np.genfromtxt('./instancias/segmentos' + str(nn) + '-' + str(instance) + '.csv', delimiter=',')
+            barriers = []
+            for lista in segments:
+                barriers.append([[lista[0], lista[1]], [lista[2], lista[3]]])
 
-                barriers = []
-                for lista in segments:
-                    barriers.append([[lista[0], lista[1]], [lista[2], lista[3]]])
+            bolas = np.genfromtxt('./instancias/bolas' + str(nn) + '-' + str(instance) + '.csv', delimiter=',')
 
-                bolas = np.genfromtxt('./instancias/bolas' + str(nn) + '-' + str(instance) + '.csv', delimiter=',')
+            N = [Circle(center=[centro1, centro2], radii=radio, col = 'blue') for centro1, centro2, radio in bolas]
 
-                N = [Circle(center=[centro1, centro2], radii=radio, col = 'blue') for centro1, centro2, radio in bolas]
+            resultados = h_kmedian_n(barriers, sources=N, targets=N, k = k, wL = wL, time_limit=time_limit)
 
-                resultados = h_kmedian_n(barriers, sources=N, targets=N, k = k, endurance=endurance, wE = wE, wL = wL, time_limit=time_limit)
+            serie = pd.Series([instance] + resultados, index=dataframe.columns)
 
-                serie = pd.Series([instance] + resultados, index=dataframe.columns)
-
-                dataframe = dataframe.append(serie, ignore_index=True)
-                dataframe.to_csv('./resultados/results_nolazy_A4.csv')
+            dataframe = dataframe.append(serie, ignore_index=True)
+            dataframe.to_csv('./resultados/results_nolazy_A4.csv')
