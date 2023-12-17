12f4278ae379afae870ca8073af1397230a8a5e0
Mini PC <z72vamac@uco.es>
2023-12-17 17:28:21 +0100

    Reordenando carpeta .tex.

--- resultados/handling_results.py
+++ resultados/handling_results.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib
 import tikzplotlib
 import os
+import matplotlib.patches as mpatches
 
 # cwd = os.getcwd()
 # files = os.listdir(cwd) 
@@ -29,6 +30,12 @@ results4 = pd.concat([parameters0, results4], axis=1)
 
 results = pd.concat([results0, results1, results2, results3, results4]).reset_index()
 
+results.rename(columns={'single':'approach'}, inplace=True)
+
+results['approach'] = results['approach'].astype('category')
+results['approach'] = results['approach'].replace({True:'single', False:'multi'})
+results['approach'].cat.reorder_categories(['single', 'multi'])
+
 # results['index'] = np.arange(len(results))
 
 results['Gap'] *= 100
@@ -36,7 +43,7 @@ results['Gap'] *= 100
 # results.loc[results['init'] == True, 'Runtime'] = results.loc[results['init']== True, 'Runtime'].fillna(3600)
 vector = ['Gap' , 'Runtime','NodeCount','ObjVal','Runtime_h','Runtime_h2','ObjVal_h','ObjVal_h2']
 
-results.loc[results['init'] == True, vector] = results.loc[results['init'] == True].groupby(['n_N', 'perc_B', 'k', 'single', 'wL','lazy','A4']).transform(lambda x: x.fillna(x.mean())).reset_index().loc[:, vector]
+results.loc[results['init'] == True, vector] = results.loc[results['init'] == True].groupby(['n_N', 'perc_B', 'k', 'approach', 'wL','lazy','A4']).transform(lambda x: x.fillna(x.mean())).reset_index().loc[:, vector]
 
 # results.loc[results['init'] == True, 'NodeCount'] = results.loc[results['init']== True, 'NodeCount'].fillna(method='ffill', limit=100)
 
@@ -48,13 +55,33 @@ results.loc[results['init'] == True, vector] = results.loc[results['init'] == Tr
 
 results.to_csv('resultados/results_together.csv')
 
-g = sns.catplot(x='n_N', y='Gap', hue='single', col='wL', kind='box', data=results, aspect=1, sharey=True)
-
 sns.set(style="darkgrid")
 
-
+matplotlib.rcParams["font.sans-serif"] = 'Times New Roman'
+matplotlib.rcParams["font.serif"] = 'Times New Roman'
 matplotlib.rcParams['axes.unicode_minus'] = False
+matplotlib.rcParams['text.usetex'] = True
 
-tikzplotlib.save('tex_files/figures/difference_between_wL.tex', encoding='utf-8')
+g = sns.catplot(x='n_N', y='Gap', hue='approach', col='wL', kind='box',  hue_order=['single', 'multi'], legend=False, data=results, sharey=True, palette="Blues")
 
+# sns.move_legend(g, "lower right", bbox_to_anchor=(-2, -1))
+
+tikzplotlib.save('resultados/difference_between_wL(gap).tex', encoding='utf-8')
+
+g.set(xlabel = r"$|\mathcal{N}|$",
+       ylabel = '\% Gap',
+       ylim = (0, 100))
+
+# ax.set_yticklabels(np.arange(0, 100, 10))
+# g._legend.set_title("Approach")
+
+for ax, title in zip(g.axes.flat, [r"$\omega_{L}=0$", r"$\omega_{L}=50$"]):
+    ax.set_title(title)
+
+lightblue_patch = mpatches.Patch(color='#B2CDDE', label='Single-commodity')
+blue_patch = mpatches.Patch(color='#4884AF', label='Multi-commodity')
+
+plt.legend(handles=[lightblue_patch, blue_patch], title = 'approach', loc='upper right', frameon = False)
+
+plt.tight_layout()
 plt.show()
